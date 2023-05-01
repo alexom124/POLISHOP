62eff7e96224dc91c5ed1b79023c2266edb16090
Alexom124 <alexom124@hotmail.com>
2023-04-30 20:12:38 -0600

    firts commit

---
+++ source/data.js
@@ -0,0 +1,28 @@
+let shopItemsData = [
+    {
+    id:"asd",
+    name:"Gorra Ipn",
+    price:75,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/ipngorra.webp"
+
+},{
+    id:"asd2",
+    name:"Playera Hombre Ipn",
+    price:130,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/ipnPlayera1.webp"
+},{
+    id:"asd3",
+    name:"Player Mujer Ipn",
+    price:240,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/ipnPlayera2.webp"
+},{
+    id:"asd4",
+    name:"Playera Unisex Ipn",
+    price:55,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/ipnPlayera3.webp"
+},
+];
